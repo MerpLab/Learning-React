3b311156b8b2dfda568d4c3020f47ebfac00becf
Maximiliano Romero <maximiliano.romero@zenithtek.io>
2024-04-02 20:54:38 -0300

    Finished shopping cart

---
+++ projects/06-shopping-cart/src/App.jsx
@@ -0,0 +1,24 @@
+import { Cart } from './components/Cart.jsx'
+import { Footer } from './components/Footer.jsx'
+import { Header } from './components/Header.jsx'
+import { Products } from './components/Products.jsx'
+import { CartProvider } from './context/cart.jsx'
+import { useFilters } from './hooks/useFilters.js'
+import { products as initialProducts } from './mocks/products.json'
+
+function App() {
+  const { filterProducts } = useFilters()
+
+  const filteredProducts = filterProducts(initialProducts)
+
+  return (
+    <CartProvider>
+      <Header />
+      <Cart />
+      <Products products={filteredProducts} />
+      <Footer />
+    </CartProvider>
+  )
+}
+
+export default App
